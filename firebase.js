3b94a1471c557b78e863d78ba265876e9d610229
All in one <ka6921830@gmail.com>
2025-08-27 04:40:44 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAbD5ocywvMleJIUM4Gmi73M7vU3M6NKDs",
+  authDomain: "all-in-one-ea8c9.firebaseapp.com",
+  projectId: "all-in-one-ea8c9",
+  storageBucket: "all-in-one-ea8c9.firebasestorage.app",
+  messagingSenderId: "209969913094",
+  appId: "1:209969913094:web:fa767d2c5cf9a10a24d287",
+  measurementId: "G-9RK2BNMX5R"
+};
